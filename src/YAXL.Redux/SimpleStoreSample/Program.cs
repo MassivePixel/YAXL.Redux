2efa601e4ec86d30a9c500a3ee9dc70b8068439b
Toni Petrina <tpetrina@tpetrina.com>
2016-05-23 09:08:01 +0200

    First draft

---
+++ src/YAXL.Redux/SimpleStoreSample/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using YAXL.Redux;
+
+namespace SimpleStoreSample
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			var store = new Store<int> ((state, action) =>
+			{
+				if (action is int) return (int)action;
+				if (action is string && (string)action == "+") return state + 1;
+				if (action is string && (string)action == "-") return state - 1;
+
+				return state;
+			}, 10);
+			Console.WriteLine ($"Initial state = {store.State}");
+
+			store.Subscribe += number => Console.WriteLine ($"State changed to {number}");
+			store.Dispatch ("+");
+			store.Dispatch ("-");
+			store.Dispatch (8);
+			store.Dispatch ("-");
+			store.Dispatch ("-");
+
+			Middleware<int> m = (state, dispatch) => next => action =>
+			{
+				Console.WriteLine ("Hello");
+				var result = next (action);
+				return result;
+			};
+
+			var store2 = Store<int>.CreateStore ((state, action) => state, 0,
+												 Store<int>.ApplyMiddleware (m));
+
+			store2.Dispatch (null);
+		}
+	}
+}
---
+++ src/YAXL.Redux/YAXL.Redux/Store.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YAXL.Redux
+{
+	using ActionType = System.Object;
+
+	public delegate ActionType DispatchDelegate (ActionType action);
+	public delegate T Reducer<T> (T state, ActionType action);
+
+	public delegate Store<T> CreateStoreDelegate<T> (Reducer<T> reducer, T initialState, Enhancer<T> enhancer);
+	public delegate Func<Reducer<T>, T, Store<T>> Enhancer<T> (CreateStoreDelegate<T> createStore);
+
+	public delegate DispatchDelegate MiddlewareApplier (DispatchDelegate app);
+	public delegate MiddlewareApplier Middleware<T> (Func<T> state, DispatchDelegate dispatch);
+
+	public class Store<T>
+	{
+		bool isDispatching;
+		Reducer<T> reducer;
+
+		public T State { get; private set; }
+		public DispatchDelegate Dispatch { get; private set; }
+
+		public delegate void SubscribeDelegate (T state);
+		public event SubscribeDelegate Subscribe;
+
+		public Store (Reducer<T> reducer, T initialState)
+		{
+			this.reducer = reducer;
+			State = initialState;
+			Dispatch = DoDispatch;
+		}
+
+		private Store (Store<T> other)
+		{
+			this.reducer = other.reducer;
+			this.State = other.State;
+			this.Dispatch = other.Dispatch;
+		}
+
+		ActionType DoDispatch (ActionType action)
+		{
+			if (isDispatching)
+				throw new InvalidOperationException ("Cannot dispatch while dispatching");
+
+			try
+			{
+				isDispatching = true;
+				State = reducer (State, action);
+			}
+			finally
+			{
+				isDispatching = false;
+			}
+
+			Subscribe?.Invoke (State);
+
+			return action;
+		}
+
+		public Store<T> WithDispatch (DispatchDelegate dispatch)
+			=> new Store<T> (this)
+			{
+				Dispatch = dispatch
+			};
+
+		public static Store<T> CreateStore (Reducer<T> reducer, T initialState, Enhancer<T> enhancer = null)
+		{
+			if (enhancer != null)
+				return enhancer (CreateStore) (reducer, initialState);
+
+			return new Store<T> (reducer, initialState);
+		}
+
+		public static MiddlewareApplier Compose (List<MiddlewareApplier> funcs)
+		{
+			if (funcs == null || funcs.Count == 0)
+				return dispatch => dispatch;
+			if (funcs.Count == 1)
+				return d => funcs [0] (d);
+
+			return d =>
+			{
+				var action = funcs [funcs.Count - 1] (d);
+				return funcs.Take (funcs.Count - 1)
+							.Aggregate (action, (arg1, arg2) => arg2 (arg1));
+			};
+		}
+
+		public static Enhancer<T> ApplyMiddleware (params Middleware<T> [] middlewares)
+		{
+			return (createStore) => (reducer, initialState) =>
+			{
+				var store = createStore (reducer, initialState, null);
+				if (middlewares == null || middlewares.Length == 0)
+					return store;
+
+				var dispatch = store.Dispatch;
+
+				var chain = middlewares.Select (m => m (() => store.State, dispatch))
+									   .ToList ();
+				var newDispatch = Compose (chain) (dispatch);
+
+				return store.WithDispatch (newDispatch);
+			};
+		}
+	}
+}
