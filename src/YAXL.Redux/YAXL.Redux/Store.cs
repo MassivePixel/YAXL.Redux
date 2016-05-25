9206b6e213e6ad054a72e5f0275e5ec2b0bb041a
Toni Petrina <tpetrina@tpetrina.com>
2016-05-25 16:49:41 +0200

    Extracted middleware into separate file

--- src/YAXL.Redux/YAXL.Redux/Store.cs
+++ src/YAXL.Redux/YAXL.Redux/Store.cs
@@ -14,10 +14,7 @@ namespace YAXL.Redux
 	public delegate Store<T> CreateStoreDelegate<T> (Reducer<T> reducer, T initialState, Enhancer<T> enhancer);
 	public delegate Func<Reducer<T>, T, Store<T>> Enhancer<T> (CreateStoreDelegate<T> createStore);
 
-	public delegate DispatchDelegate MiddlewareApplier (DispatchDelegate app);
-	public delegate MiddlewareApplier Middleware<T> (Func<T> state, DispatchDelegate dispatch);
-
-	public class Store<T>
+	public partial class Store<T>
 	{
 		bool isDispatching;
 		Reducer<T> reducer;
@@ -74,39 +71,6 @@ namespace YAXL.Redux
 				return enhancer (CreateStore) (reducer, initialState);
 
 			return new Store<T> (reducer, initialState);
-		}
-
-		public static MiddlewareApplier Compose (List<MiddlewareApplier> funcs)
-		{
-			if (funcs == null || funcs.Count == 0)
-				return dispatch => dispatch;
-			if (funcs.Count == 1)
-				return d => funcs [0] (d);
-
-			return d =>
-			{
-				var action = funcs [funcs.Count - 1] (d);
-				return funcs.Take (funcs.Count - 1)
-							.Aggregate (action, (arg1, arg2) => arg2 (arg1));
-			};
-		}
-
-		public static Enhancer<T> ApplyMiddleware (params Middleware<T> [] middlewares)
-		{
-			return (createStore) => (reducer, initialState) =>
-			{
-				var store = createStore (reducer, initialState, null);
-				if (middlewares == null || middlewares.Length == 0)
-					return store;
-
-				var dispatch = store.Dispatch;
-
-				var chain = middlewares.Select (m => m (() => store.State, dispatch))
-									   .ToList ();
-				var newDispatch = Compose (chain) (dispatch);
-
-				return store.WithDispatch (newDispatch);
-			};
 		}
 	}
 }
