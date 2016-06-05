de357589d6dcb7f415164b084ac81d67fa9e1eae
Toni Petrina <tpetrina@tpetrina.com>
2016-06-05 23:14:45 +0200

    Added sample for combining reducers

---
+++ samples/ComposeReducers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComposeReducers
+{
+	public class CombinedStore
+	{
+		public int Counter1 { get; set; }
+		public int Counter2 { get; set; }
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Func<int, object, int> reducer = (state, action) => state;
+
+			var store = YAXL.Redux.Store<CombinedStore>.CreateStore(
+				(state, action) => new CombinedStore
+				{
+					Counter1 = reducer(state.Counter1, action),
+					Counter2 = reducer(state.Counter2, action)
+				},
+				new CombinedStore
+				{
+					Counter1 = 0,
+					Counter2 = 0
+				});
+
+			store.Dispatch(null);
+		}
+	}
+}
