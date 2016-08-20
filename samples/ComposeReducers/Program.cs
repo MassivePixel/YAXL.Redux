afec1afc4acc10c577e9b0db0754d8d4a3eeadd8
Toni Petrina <tpetrina@tpetrina.com>
2016-08-20 15:24:43 +0200

    Updated sample with combined reducers

--- samples/ComposeReducers/Program.cs
+++ samples/ComposeReducers/Program.cs
@@ -1,32 +1,66 @@
-﻿using System;
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using YAXL.Redux;
 
 namespace ComposeReducers
 {
-	public class CombinedStore
-	{
-		public int Counter1 { get; set; }
-		public int Counter2 { get; set; }
-	}
+    public class CombinedStore
+    {
+        public int Counter1 { get; set; }
+        public int Counter2 { get; set; }
+    }
 
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Func<int, object, int> reducer = (state, action) => state;
+    public class CombinedStore2
+    {
+        public CombinedStore Store1 { get; set; }
+        public CombinedStore Store2 { get; set; }
+        public int Counter1 { get; set; }
 
-			var store = YAXL.Redux.Store<CombinedStore>.CreateStore(
-				(state, action) => new CombinedStore
-				{
-					Counter1 = reducer(state.Counter1, action),
-					Counter2 = reducer(state.Counter2, action)
-				},
-				new CombinedStore
-				{
-					Counter1 = 0,
-					Counter2 = 0
-				});
+        public static CombinedStore2 Reducer(CombinedStore2 state, object action)
+            => new CombinedStore2
+            {
+            };
+    }
 
-			store.Dispatch(null);
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Func<int, object, int> reducer = (state, action) => state;
+            Func<CombinedStore, object, CombinedStore> reducer2 = (state, action) => state;
+
+            // if state becomes to large for a single reducer, one can always
+            // create a meta reducer that will combine separate reducer-based
+            // stores into a large state
+            var store = Store<CombinedStore>.CreateStore(
+                (state, action) => new CombinedStore
+                {
+                    Counter1 = reducer(state.Counter1, action),
+                    Counter2 = reducer(state.Counter2, action)
+                },
+                new CombinedStore());
+
+            // recursion!
+            // no matter how complex your state is, you can always subdivide it
+            var store2 = new Store<CombinedStore2>((state, action) =>
+                new CombinedStore2
+                {
+                    Store1 = reducer2(state?.Store1, action),
+                    Store2 = reducer2(state?.Store2, action),
+                    Counter1 = reducer(state?.Counter1 ?? 0, action)
+                });
+
+            // to simplify the code above, reducer can be brought from elsewhere
+            var store3 = new Store<CombinedStore2>(CombinedStore2.Reducer);
+            var store4 = Store.Create(CombinedStore2.Reducer, new CombinedStore2());
+
+            store.Dispatch(null);
+        }
+    }
+
+    public static class Store
+    {
+        public static Store<T> Create<T>(Reducer<T> reducer, T initialState) => new Store<T>(reducer, initialState);
+    }
 }
--- src/YAXL.Redux/Store.cs
+++ src/YAXL.Redux/Store.cs
@@ -1,69 +1,68 @@
 ﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace YAXL.Redux
 {
-	using ActionType = System.Object;
+    // All actions are actually objects
+    using ActionType = System.Object;
 
-	public delegate ActionType Dispatcher (ActionType action);
-	public delegate T Reducer<T> (T state, ActionType action);
+    public delegate ActionType Dispatcher(ActionType action);
+    public delegate T Reducer<T>(T state, ActionType action);
 
-	public delegate Store<T> StoreCreator<T> (Reducer<T> reducer, T initialState, Enhancer<T> enhancer);
-	public delegate StoreCreator<T> Enhancer<T> (StoreCreator<T> createStore);
+    public delegate Store<T> StoreCreator<T>(Reducer<T> reducer, T initialState, Enhancer<T> enhancer);
+    public delegate StoreCreator<T> Enhancer<T>(StoreCreator<T> createStore);
 
-	public partial class Store<T>
-	{
-		bool isDispatching;
-		Reducer<T> reducer;
+    public partial class Store<T>
+    {
+        bool isDispatching;
+        Reducer<T> reducer;
 
-		public T State { get; private set; }
-		public Dispatcher Dispatch { get; private set; }
+        public T State { get; private set; }
+        public Dispatcher Dispatch { get; private set; }
 
-		public delegate void Subscriber (T state);
-		public event Subscriber Subscribe;
+        public delegate void Subscriber(T state);
+        public event Subscriber Subscribe;
 
-		public Store (Reducer<T> reducer, T initialState)
-		{
-			this.reducer = reducer;
-			State = initialState;
-			Dispatch = DoDispatch;
-		}
+        public Store(Reducer<T> reducer, T initialState = default(T))
+        {
+            this.reducer = reducer;
+            State = initialState;
+            Dispatch = DoDispatch;
+        }
 
-		ActionType DoDispatch (ActionType action)
-		{
-			if (isDispatching)
-				throw new InvalidOperationException ("Cannot dispatch while dispatching");
+        ActionType DoDispatch(ActionType action)
+        {
+            if (isDispatching)
+                throw new InvalidOperationException("Cannot dispatch while dispatching");
 
-			try
-			{
-				isDispatching = true;
-				State = reducer (State, action);
-			}
-			finally
-			{
-				isDispatching = false;
-			}
+            try
+            {
+                isDispatching = true;
+                State = reducer(State, action);
+            }
+            finally
+            {
+                isDispatching = false;
+            }
 
-			Subscribe?.Invoke (State);
+            Subscribe?.Invoke(State);
 
-			return action;
-		}
+            return action;
+        }
 
-		public Store<T> ReplaceDispatch (Dispatcher dispatch)
-		{
-			Dispatch = dispatch;
-			return this;
-		}
+        public Store<T> ReplaceDispatch(Dispatcher dispatch)
+        {
+            Dispatch = dispatch;
+            return this;
+        }
 
-		public static Store<T> CreateStore (Reducer<T> reducer, T initialState, Enhancer<T> enhancer = null)
-		{
-			if (enhancer != null)
-				return enhancer (CreateStore) (reducer, initialState, null);
+        public static Store<T> CreateStore(Reducer<T> reducer, T initialState, Enhancer<T> enhancer = null)
+        {
+            if (enhancer != null)
+                return enhancer(CreateStore)(reducer, initialState, null);
 
-			return new Store<T> (reducer, initialState);
-		}
-	}
+            return new Store<T>(reducer, initialState);
+        }
+    }
 }
