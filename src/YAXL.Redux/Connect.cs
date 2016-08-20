dabf4edb645ba4c12bc5b31349abcefd72e6ce40
Toni Petrina <tpetrina@tpetrina.com>
2016-08-20 16:51:14 +0200

    Added connect function

---
+++ src/YAXL.Redux/Connect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace YAXL.Redux
+{
+    public partial class Store<T>
+    {
+        class Unsubscriber : IDisposable
+        {
+            Action unsubscribeAction;
+
+            public Unsubscriber(Action unsubscribeAction)
+            {
+                if (unsubscribeAction == null)
+                    throw new ArgumentNullException(nameof(unsubscribeAction));
+
+                this.unsubscribeAction = unsubscribeAction;
+            }
+
+            public void Dispose()
+            {
+                unsubscribeAction?.Invoke();
+                unsubscribeAction = null;
+            }
+        }
+
+        public IDisposable Connect<TProps>(Func<T, TProps> mapStateToProps, Action<TProps> propsChanged)
+        {
+            if (mapStateToProps == null)
+                throw new ArgumentNullException(nameof(mapStateToProps));
+            if (propsChanged == null)
+                throw new ArgumentNullException(nameof(propsChanged));
+
+            var lastProps = mapStateToProps(State);
+            propsChanged(lastProps);
+
+            Subscriber subscription = (state) =>
+            {
+                var newProps = mapStateToProps(state);
+                if (!lastProps.Equals(newProps))
+                {
+                    propsChanged?.Invoke(newProps);
+                    lastProps = newProps;
+                }
+            };
+
+            Subscribe += subscription;
+
+            return new Unsubscriber(() => Subscribe -= subscription);
+        }
+    }
+}
+
