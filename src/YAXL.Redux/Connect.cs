5db209c848de9649cea02bb2a14e72c3dde1df2c
Toni Petrina <tpetrina@tpetrina.com>
2016-11-09 13:08:59 -0800

    Updated license

--- src/YAXL.Redux/Connect.cs
+++ src/YAXL.Redux/Connect.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
 
 namespace YAXL.Redux
 {
